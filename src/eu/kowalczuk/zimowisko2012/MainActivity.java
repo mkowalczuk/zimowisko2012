c3822a2c2c23d1a317c446cd895462059e7f861c
Michał Kowalczuk <michal@kowalczuk.eu>
2012-01-10 19:35:45 +0100

    converted MainActivity to extend ListActivity

--- src/eu/kowalczuk/zimowisko2012/MainActivity.java
+++ src/eu/kowalczuk/zimowisko2012/MainActivity.java
@@ -1,9 +1,9 @@
 package eu.kowalczuk.zimowisko2012;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
