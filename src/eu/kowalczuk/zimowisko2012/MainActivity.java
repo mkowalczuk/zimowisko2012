67e66a0943c439258d8dabf154f5f2922752a768
Michał Kowalczuk <michal@kowalczuk.eu>
2012-01-10 19:28:06 +0100

    initial commit

---
+++ src/eu/kowalczuk/zimowisko2012/MainActivity.java
@@ -0,0 +1,13 @@
+package eu.kowalczuk.zimowisko2012;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
